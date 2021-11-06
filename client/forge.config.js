c5e91312fff05f9c9fbc599b3d4c8e9ae06cb169
Justin Vanderhooft <justinvdhooft@gmail.com>
2021-11-06 19:22:25 -0400

    feat: scaffold the basic electron app and dmg build (#2)

---
+++ client/forge.config.js
@@ -0,0 +1,58 @@
+module.exports = {
+  packagerConfig: {
+    asar: true,
+    ignore:
+      '\\.git(ignore|modules)|node_modules/(\\.cache|.cli-ngcc)|\\.vscode|e2e|\\.editorconfig|\\.eslintrc\\.json|\\.npmrc|angular\\.json|angular\\.webpack\\.js|main\\.js\\.map|main\\.ts|tsconfig\\.json|tsconfig\\.serve\\.json|\\.env',
+    darwinDarkModeSupport: 'true',
+    name: 'Sub Dazzle',
+    executableName: 'SubDazzle',
+    osxSign: {
+      entitlements: './bin/entitlements.plist',
+      'entitlements-inherit': './bin/entitlements.plist',
+      'gatekeeper-assess': false,
+      hardenedRuntime: true,
+      identity: 'Developer ID Application: Justin Vanderhooft (2RSKA7RG4C)',
+      'signature-flags': 'library'
+    },
+    osxNotarize: {
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_ID_PASSWORD
+    },
+    extendInfo: {
+      NSAppTransportSecurity: {
+        NSAllowsArbitraryLoads: false,
+        NSExceptionDomains: {
+          'downloads.subdazzle.app': {
+            NSTemporaryExceptionAllowsInsecureHTTPSLoads: false,
+            NSIncludesSubdomains: false,
+            NSTemporaryExceptionAllowsInsecureHTTPLoads: true,
+            NSTemporaryExceptionMinimumTLSVersion: '1.0',
+            NSTemporaryExceptionRequiresForwardSecrecy: false
+          }
+        }
+      }
+    }
+  },
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        name: 'sub_dazzle'
+      }
+    },
+    {
+      name: '@electron-forge/maker-dmg',
+      config: {
+        format: 'ULFO'
+      }
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {}
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {}
+    }
+  ]
+};
---
+++ client/main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+// modify your existing createWindow() function
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
+
+app.on('window-all-closed', function () {
+  // For now, we just quit the app when the window is closed even on macOS
+  // if (process.platform !== 'darwin') app.quit()
+})
+
+// app.whenReady().then(() => {
+//   createWindow()
+
+//   app.on('activate', function () {
+//     if (BrowserWindow.getAllWindows().length === 0) createWindow()
+//   })
+// })
