199aeb912eb5a40c933c064d3fcb9ef4590cc8f0
Brian R <dudewheresmycode@users.noreply.github.com>
2021-11-06 20:22:56 -0600

    Add react app (#3)

--- electron/main.js
+++ electron/main.js
@@ -1,27 +1,33 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 // modify your existing createWindow() function
-function createWindow () {
+function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
-  })
+  });
 
-  win.loadFile('index.html')
+  if (process.env.NODE_ENV === 'production') {
+    // load static production build
+    win.loadFile(path.join(__dirname, '../client/build/index.html'));
+  } else {
+    // load dev server
+    win.loadURL('http://localhost:3000');
+  }
 }
 
 app.whenReady().then(() => {
-  createWindow()
-})
+  createWindow();
+});
 
 app.on('window-all-closed', function () {
   // For now, we just quit the app when the window is closed even on macOS
   // if (process.platform !== 'darwin') app.quit()
-})
+});
 
 // app.whenReady().then(() => {
 //   createWindow()
