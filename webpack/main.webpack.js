4070f75b2d8f09b4bdcd348df988810e22c1601a
Brian Robinson <brian@ndmweb.com>
2021-11-07 20:56:24 -0700

    fixup! remove cruft

--- webpack/main.webpack.js
+++ webpack/main.webpack.js
@@ -1,8 +1,5 @@
-const path = require('path');
-
 module.exports = {
   resolve: {
-    // modules: [path.resolve(__dirname, 'core'), 'node_modules'],
     extensions: ['.ts', '.js']
   },
   entry: './core/main.ts',
